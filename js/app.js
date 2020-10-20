160a51a9c4f7ba11d97fcf8e3417dd99ae58521e
bak1roff01 <bakirovgulomjon@gmail.com>
2020-10-21 01:03:40 +0300

    Create cuba-phot-slider section and some scroll action

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	AOS.init();
 	$('.loading').css("display" , "none")
 	$('#nav-icon3').click(function(){
 		$(this).toggleClass('open');
@@ -6,5 +8,13 @@ $(document).ready(function(){
 		$(".site-menu").toggleClass('sitenav--close');
 
 	});
+	$(function(){
+		$(".slider-photo").slick({
+			autoplay: true,
+      autoplaySpeed: 2000,
+			prevArrow: '<button type="button" class="slick-prev"><img src="../img/icon-left.svg" alt=""></button>',
+			nextArrow: '<button type="button" class="slick-next"><img src="../img/icon-right.svg" alt=""></button>'
+		})
+	})
 
 });
